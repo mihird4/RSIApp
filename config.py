03b7658f7dd6091fab03f8a4404447ff0486ba88
Mihir <mihir.da@gmail.com>
2023-02-16 14:10:15 -0500

    Update config.py
    Update secret key

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class Config:
     """Base config vars."""
-    SECRET_KEY = '\xe0\\t~\xd0m\x9c3;\x81\xcbq\xbc\xce\xe3Hd\xfc\x8a\x0e\x15\x94^6'
+    SECRET_KEY = '\xe0\\t~\xd0m\x9c3;\x81\xcbq\xbc\xce\xe3Hd\xfc\x8a\x0e\x15\x94^6-CHANGETHIS'
 
 
 class ProdConfig(Config):
